97c5151b6c38af27d16486a4fa40a8d54b3da586
AmaliaMiron <mironamalia7@gmail.com>
2022-10-01 10:43:28 +0300

    first commit

--- 5th Homework/Rock,paper,scissors/rps.js
+++ 5th Homework/Rock,paper,scissors/rps.js
@@ -1,27 +1,27 @@
 function Play() {
     
-var rPs = ["Rock", "Paper", "Scissors"];
-// We need two random variables, one for 'you'
-var user = rPs[Math.floor(Math.random() * 3)];
+var posibilities = ["Rock", "Paper", "Scissors"];
+// We need two random variables, one for 'user
+var user = posibilities [Math.floor(Math.random() * 3)];
 // and one for the 'computer'
-var computer = rPs[Math.floor(Math.random() * 3)];
+var computer = posibilities [Math.floor(Math.random() * 3)];
 
 // Now invoke function
 
-    if (computer === "Rock") {
+    if (computer === "Rock");
         console.log ("Computer choice: Rock");
-    }
+    
 
-    if (user === "Paper") {
+    if (user === "Paper");
         console.log ("User choice: Paper");
-    }
+    
 
     
-    if (user==="Rock" && computer === "Scissors" || user === "Paper" && computer==="Rock"||user==="Scissors"&&computer==="Paper"){
+    if (user==="Rock" && computer === "Scissors") || (user === "Paper" && computer==="Rock")||(user==="Scissors"&&computer==="Paper"); 
          console.log ("User wins!");
-    } 
-    else {
+     
+        else {
          console.log ("You've lost!");
-    }
+        }
 }
     
